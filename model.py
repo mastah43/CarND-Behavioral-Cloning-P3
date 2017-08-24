4d5d863d3774c3943f30b5f409d526195b52a491
Marc Neumann <marc.neumann@bmw.de>
2017-08-24 19:50:56 +0200

    changed activation for final layers

--- model.py
+++ model.py
@@ -85,19 +85,19 @@ def create_model_nvidia():
     
     model.add(Flatten())
     model.add(Dense(1164))
-    model.add(Activation('elu'))
+    model.add(Activation('linear'))
     model.add(Dropout(0.5))
 
     model.add(Dense(100))
-    model.add(Activation('elu'))
+    model.add(Activation('linear'))
     model.add(Dropout(0.5))
 
     model.add(Dense(50))
-    model.add(Activation('elu'))
+    model.add(Activation('linear'))
     model.add(Dropout(0.5))
 
     model.add(Dense(10))
-    model.add(Activation('elu'))
+    model.add(Activation('linear'))
 
     model.add(Dense(1, name='angle'))
 
